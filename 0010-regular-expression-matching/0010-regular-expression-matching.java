ea3ef03289623f6fdbed69ebf9d401e2672e8fbc
Anshu Kumari <anshukumari82524@gmail.com>
2026-08-08 14:58:57 +0530

    Time: 2 ms (47.67%), Space: 43.2 MB (88.12%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,49 +1,26 @@
 class Solution {
-
-    private Boolean[][] memo;
-
     public boolean isMatch(String s, String p) {
+        int m = s.length(), n = p.length();
+        // dp[i][j] = true if s[i..m) matches p[j..n)
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[m][n] = true; // empty string matches empty pattern
 
-        memo = new Boolean[s.length() + 1][p.length() + 1];
+        // Fill from bottom-right to top-left
+        for (int i = m; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                boolean firstMatch = (i < m) &&
+                        (p.charAt(j) == s.charAt(i) || p.charAt(j) == '.');
 
-        return dp(0, 0, s, p);
-    }
-
-    private boolean dp(int i, int j, String s, String p) {
-
-        // Already computed
-        if (memo[i][j] != null) {
-            return memo[i][j];
+                if (j + 1 < n && p.charAt(j + 1) == '*') {
+                    // Option 1: skip "x*" entirely (zero occurrences)
+                    // Option 2: use one occurrence of x and stay on same pattern position
+                    dp[i][j] = dp[i][j + 2] || (firstMatch && dp[i + 1][j]);
+                } else {
+                    dp[i][j] = firstMatch && dp[i + 1][j + 1];
+                }
+            }
         }
 
-        // Pattern finished
-        if (j == p.length()) {
-            return i == s.length();
-        }
-
-        // Check current match
-        boolean firstMatch = (
-            i < s.length() &&
-            (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.')
-        );
-
-        boolean ans;
-
-        // Check for '*'
-        if (j + 1 < p.length() && p.charAt(j + 1) == '*') {
-
-            // 1. Skip x*
-            // 2. Use x* if matched
-            ans = dp(i, j + 2, s, p) ||
-                  (firstMatch && dp(i + 1, j, s, p));
-
-        } else {
-
-            ans = firstMatch &&
-                  dp(i + 1, j + 1, s, p);
-        }
-
-        memo[i][j] = ans;
-        return ans;
+        return dp[0][0];
     }
 }
