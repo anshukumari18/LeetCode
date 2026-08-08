cfddccd22f467ec4c070af976784f322895581ff
Anshu Kumari <anshukumari82524@gmail.com>
2026-08-08 14:57:10 +0530

    Time: 5 ms (81.23%), Space: 46.1 MB (33.84%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,19 +1,16 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        // Negative numbers are not palindromes
-        // Numbers ending in 0 (except 0 itself) can't be palindromes
-        if (x < 0 || (x % 10 == 0 && x != 0)) {
+        if(x<0){
             return false;
         }
+        int rev = 0;
+        int  num= x;
 
-        int reverted = 0;
-        while (x > reverted) {
-            reverted = reverted * 10 + x % 10;
-            x /= 10;
+        while (num!= 0) {
+            rev= rev*10 + num%10;
+            num=num/10;
         }
 
-        // When the length is odd, we can get rid of the middle digit by reverted/10
-        // e.g. 12321 -> x=12, reverted=123 -> 123/10 == 12
-        return x == reverted || x == reverted / 10;
+        return (rev == x);
     }
 }
