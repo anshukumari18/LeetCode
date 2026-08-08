597b379eeb151d62a4dac0c997d66fb83a04a528
Anshu Kumari <anshukumari82524@gmail.com>
2026-08-08 14:40:45 +0530

    Time: 554 ms (17.36%), Space: 47 MB (21.89%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,33 +1,36 @@
-class Solution {
+public class Solution {
     public String longestPalindrome(String s) {
-        int n=s.length(),max=0,start=0,end=0;
-        String res="";
-        for(int i=0;i<n;i++){
-            //streaching odd b a b
-            int left=i,right=i;
-            while(left>=0&&right<n&&s.charAt(left)==s.charAt(right)){
-                left--;
-                right++;
-            }
-            if((right-left+1)>max){
-                max=right-left+1;
-                start=left+1;
-                end=right;
-            }
+        if (s.length() <= 1) {
+            return s;
+        }
 
-            //even length b|b
-            left=i;
-            right=i+1;
-            while(left>=0&&right<n&&s.charAt(left)==s.charAt(right)){
-                left--;
-                right++;
-            }
-            if((right-left+1)>max){
-                max=right-left+1;
-                start=left+1;
-                end=right;
+        int maxLen = 1;
+        String maxStr = s.substring(0, 1);
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + maxLen; j <= s.length(); j++) {
+                if (j - i > maxLen && isPalindrome(s.substring(i, j))) {
+                    maxLen = j - i;
+                    maxStr = s.substring(i, j);
+                }
             }
         }
-        return s.substring(start,end);
+
+        return maxStr;
+    }
+
+    private boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
     }
 }
