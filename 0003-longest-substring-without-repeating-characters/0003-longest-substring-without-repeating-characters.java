d65c4ae6a0dcb6a4a0179f712c84c61b61a7e3e8
Anshu Kumari <anshukumari82524@gmail.com>
2026-08-10 12:26:05 +0530

    Time: 67 ms (12.56%), Space: 47.7 MB (18.03%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,40 +1,16 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        // We'll use a "set" to remember which letters we've seen in the current substring.
-        // Think of it as a VIP list for letters - no duplicates allowed! 🚫
-        Set<Character> set = new HashSet<>();
-
-        // This will keep track of the longest substring we've found so far.
-        int maxLen = 0;
-
-        // 'left' will point to the beginning of our current substring.
-        // It's like marking where our treasure hunt starts. 🗺️
-        int left = 0;
-
-        // This loop goes through the entire string, letter by letter.
-        // 'right' is our explorer, checking out each letter one by one. 🚶
-        for (int right = 0; right < s.length(); right++) {
-            // Get the current letter that the explorer is looking at.
-            char c = s.charAt(right);
-
-            // Uh oh! We've already seen this letter in our current substring! 😱
-            // We need to shrink our substring from the left until we don't have duplicates anymore.
-            while (set.contains(c)) {
-                // Remove the leftmost letter from our set.
-                set.remove(s.charAt(left));
-                // Move the 'left' pointer one step to the right, shrinking our substring.
-                left++;
+        HashSet <Character> set=new HashSet<>();
+        int l=0;
+        int max=0;
+        for(int r=0;r<s.length();r++){
+            while(set.contains(s.charAt(r))){
+                set.remove(s.charAt(l));
+                l++;
             }
-
-            // Now that we've removed any duplicates, we can safely add the current letter to our set.
-            set.add(c);
-
-            // Check if our current substring is longer than the longest one we've found so far.
-            // If it is, update 'maxLen'.
-            maxLen = Math.max(maxLen, right - left + 1);
+            set.add(s.charAt(r));
+            max=Math.max(max,r-l+1);
         }
-
-        // Finally, return the length of the longest substring we found. 🎉
-        return maxLen;
+        return max;
     }
 }
