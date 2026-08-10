44c06adff61e3379050ae63c3afd9c6da4b65fde
Anshu Kumari <anshukumari82524@gmail.com>
2026-08-10 12:38:44 +0530

    Time: 5 ms (83.01%), Space: 77.5 MB (29.66%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,17 +1,13 @@
 class Solution {
-    public int maxArea(int[] arr) {
-        int max = 0 , i = 0, j = arr.length - 1;
-        while(i < j){
-            int minH = Math.min(arr[i], arr[j]);
-            int test = (j - i) * minH;
-            if(max < test) max = test;
-            // 2 pointers...
-            // Shift the pointer with the smaller height because
-            // it has already contributed the maximum it could.
-            //
-            // Shift either pointer if they have the same height.
-            if(arr[i] <= arr[j]) i++ ;
-            else j--;
+    public int maxArea(int[] height) {
+        int l = 0, r = height.length - 1;
+        int max = 0;
+
+        while (l < r) {
+            int area = (r - l) * Math.min(height[l], height[r]);
+            max = Math.max(max, area);
+            if (height[l] < height[r]) l++;
+            else r--;
         }
         return max;
     }
