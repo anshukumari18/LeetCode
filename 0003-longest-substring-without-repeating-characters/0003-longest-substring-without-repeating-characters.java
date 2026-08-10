691a55c57c40f739d1359929b1ee55ebc223ecf6
Anshu Kumari <anshukumari82524@gmail.com>
2026-08-10 12:20:40 +0530

    Time: 4 ms (90.94%), Space: 48 MB (5.54%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,25 +1,81 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-       int l = 0, r = 0;
-       int len = 0, maxlen = 0;
-       int hash[] = new int[256];
-       java.util.Arrays.fill(hash, -1);
+        int n = s.length();
+        if (n <= 1) return n;
+        int[] charIdx = new int[128]; //stores idxs of char when the appear
+        int start = 0;
+        int globalLen = 0;
+    
+        for (int i=0;i<n;i++){
+            char ch = s.charAt(i);
+            int preIdx = charIdx[(int)ch];
+            start = Math.max(start, preIdx );
+            globalLen = Math.max(i-start+1, globalLen);
+            charIdx[(int) ch] = i + 1 ; //storing pos of next start if this element is found to avoid default zero problem
+        }
+        return globalLen;
+}}
 
-       for(int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
+// //while loop to find start is not optimal - but still a good solution
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         if (s.length() <= 1) return s.length();
+//         HashSet<Character> hs = new HashSet<>();
 
-            if(hash[c] != -1) {
-                if(hash[c] >= l) {
-                    l = hash[c] + 1;
+//         int start = 0;
+//         int globalLen = 0;
+//         int windowLen = 0;
+
+//         for (Character c: s.toCharArray()){
+//             if (hs.contains(c)){
+//                 globalLen = Math.max(windowLen, globalLen);
+//                 //bring start to the point at which char existed before
+//                 while (s.charAt(start) != c){
+//                     hs.remove(s.charAt(start)); //removing elemets is important
+//                     start++;
+//                     windowLen--;
+//                 }
+//                 start++;
+//             }else{
+//                 hs.add(c);
+//                 windowLen++;
+//             }
+//         }
+//         return Math.max(globalLen,windowLen) ;
+//     }
+// }
+
+/*old code
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length() < 2) return s.length();
+        Map<Character, Integer > hm = new HashMap<>();
+        int max = 0;
+        int start = 0;
+        int i = 0;
+        for (Character c: s.toCharArray()){
+            if (hm.containsKey(c)){
+                int localMax = i - start;
+                if (localMax > max){
+                    max = localMax;
                 }
+                
+                start = hm.get(c) + 1 < start ? start :hm.get(c) + 1 ;
             }
+            hm.put(c,i);
+            i++;
+        }
+        int localMax = i - start;
+        if (localMax > max){
+            max = localMax;
+        }
 
-            len = r-l+1;
-            maxlen = Math.max(len, maxlen);
-            hash[c] = r;
-            r++;
-       }
 
-        return maxlen;
+        return max;
+
+        
     }
 }
+
+
+*/
