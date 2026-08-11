fff0989220955e081710fe64ce0dbeb3268df870
Anshu Kumari <anshukumari82524@gmail.com>
2026-08-11 20:25:14 +0530

    Time: 0 ms (100%), Space: 46.8 MB (43.85%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,25 +1,16 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int n = nums.length;
 
-        int low = 0; // pointer starts at index 0 ---> it points to the last unique element encountered
-        int high = 1; // pointer starts from index 1 - because index 0 is unique always ---> loops forward to find the next unique element on the basis of comparison with the current low value
-        int uniqueCount = 1; // the first element will always be unique
-
-        while (high < n) { // the function can run until the last index of the array and then terminated
-            // if the current unique value and the encountered value are equal ---> it's a duplicate --> increment high --> continue
-            if(nums[high] == nums[low]){
-                high ++;
-                continue;
-            }
-            // if the current unique value and the encountered value are NOT equal ---> it's a new unique --> increment low --> increment uniqueCount --> increment high 
-            else{
-                nums[low + 1] = nums[high]; // place the new unique element in the index next to low --> [low+1]
-                low++; 
-                uniqueCount++;
-                high++;
+        int j = 0;
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[j]) {
+                j++;
+                nums[j] = nums[i];
             }
         }
-        return uniqueCount;
+        
+        return j + 1;
+        
     }
 }
