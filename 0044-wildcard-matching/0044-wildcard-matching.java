dd6617715560efe11a0e3ee57caab53dd4beb67d
Anshu Kumari <anshukumari82524@gmail.com>
2026-08-13 12:07:05 +0530

    Time: 2 ms (93.06%), Space: 44.7 MB (99.58%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,20 +1,25 @@
 class Solution {
-    public boolean isMatch(String s, String p) {
-        Boolean[][]arr=new Boolean[s.length()][p.length()];
-        return helper(s,p,arr,s.length()-1,p.length()-1);
-    }
-    public static boolean helper(String text1,String text2, Boolean[][]dp,int index1,int index2){
-        if(index1<0&&index2<0)return true;
-        if(index1>=0&&index2<0)return false;
-        if(index1<0&&index2>=0){
-            for(int i=index2;i>=0;i--){
-                if(text2.charAt(i)!='*')return false;
+    public boolean isMatch(final String s, final String p) {
+        int i = 0, j = 0, star = -1, match = -1;
+
+        while(i < s.length()) {
+            if(j < p.length() && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '?')) {
+                i++;
+                j++;
+            } else if(j < p.length() && p.charAt(j) == '*') {
+                star = j++;
+                match = i;
+            } else if(star != -1) {
+                j = star + 1;
+                i = ++match;
+            } else {
+                return false;
             }
-            return true;
         }
-        if(dp[index1][index2]!=null)return dp[index1][index2];
-        if(text1.charAt(index1)==text2.charAt(index2)||text2.charAt(index2)=='?')return dp[index1][index2]=helper(text1,text2,dp,index1-1,index2-1);
-        if(text2.charAt(index2)=='*')return dp[index1][index2]=helper(text1,text2,dp,index1-1,index2)||helper(text1,text2,dp,index1,index2-1);
-        return false;
+
+        while(j < p.length() && p.charAt(j) == '*')
+            j++;
+
+        return i == s.length() && j == p.length();
     }
 }
