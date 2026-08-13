d3e3426ea4f36876b899e8dfaa425e0d9684af89
Anshu Kumari <anshukumari82524@gmail.com>
2026-08-13 13:06:11 +0530

    Time: 7 ms (72.31%), Space: 49.5 MB (71.84%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,32 +1,16 @@
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs){
-        HashMap<String,ArrayList<String>> hm = new HashMap<>();
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String , List<String>> map = new HashMap<>();
         for(String str : strs){
-            String sortedString = sortTheString(str);
-            ArrayList<String> list = hm.get(sortedString);
-            if(list == null){
-                list = new ArrayList<String>();
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
+
+            if(!map.containsKey(key)){
+                map.put(key , new ArrayList<>());
             }
-            list.add(str);
-            hm.put(sortedString,list);
-        }
-        List<List<String>> res = new ArrayList<>();
-        for(String key : hm.keySet()){
-            res.add(hm.get(key));
-        }
-        return res;
-    }
-    private String sortTheString(String str){
-        int[] freq = new int[26];
-        for(char ch : str.toCharArray()){
-            freq[ch - 'a']++;
-        }
-        StringBuilder res = new StringBuilder("");
-        for(int i = 0 ; i < 26 ; i++){
-            while(freq[i]-- != 0){
-                res.append((char)(i + 'a'));
-            }
-        }
-        return res.toString();
+            map.get(key).add(str);
+        }  
+        return new ArrayList<>(map.values());
     }
 }
