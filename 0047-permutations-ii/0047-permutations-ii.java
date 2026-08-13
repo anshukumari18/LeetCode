a9acc1bc4ca39dca16647612c0efeb1ff727b3d4
Anshu Kumari <anshukumari82524@gmail.com>
2026-08-13 12:41:49 +0530

    Time: 74 ms (6.92%), Space: 47.1 MB (13.5%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,27 +1,27 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        Arrays.sort(nums);
         List<List<Integer>> res=new ArrayList<>();
         List<Integer> temp=new ArrayList<>();
+        Set<List<Integer>> distinct=new HashSet<>();
         Set<Integer> visited=new HashSet<>();
-        helper(0, nums, res, visited, temp);
+        helper(0, nums, distinct, visited, temp);
+        res.addAll(distinct);
         return res;
     }
 
-    public void helper(int i, int[] nums, List<List<Integer>> res, Set<Integer> visited, List<Integer> temp) {
+    public void helper(int i, int[] nums, Set<List<Integer>> distinct, Set<Integer> visited, List<Integer> temp) {
         if(i==nums.length)
         {
-            res.add(new ArrayList<>(temp));
+            distinct.add(new ArrayList<>(temp));
             return;
         }
         for(int k=0;k<nums.length;k++)
         {
-            if(k>0 &&  nums[k]==nums[k-1] && !visited.contains(k-1)) continue;
             if(!visited.contains(k))
             {
                 visited.add(k);
                 temp.add(nums[k]);
-                helper(i+1, nums, res, visited, temp);
+                helper(i+1, nums, distinct, visited, temp);
                 temp.remove(temp.size()-1);
                 visited.remove(k);
             }
