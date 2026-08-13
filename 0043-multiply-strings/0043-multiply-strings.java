f1e78a94703192a682c1a5ecd52580fa17379ec9
Anshu Kumari <anshukumari82524@gmail.com>
2026-08-13 11:54:41 +0530

    Time: 2 ms (99.81%), Space: 43.5 MB (66.62%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.java
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -1,34 +1,58 @@
 class Solution {
+
     public String multiply(String num1, String num2) {
-        int m = num1.length();
-        int n = num2.length();
-        int[] res = new int[m + n];
+        if(num1.length() == 1 && num2.length() == 1){
+            int cnt1 = num1.charAt(0) - '0';
+            int cnt2 = num2.charAt(0) - '0';
+            int ans = cnt1 * cnt2;
+            return String.valueOf(ans);
+        }
 
-        for (int i = m - 1; i >= 0; i--) {
-            int d1 = num1.charAt(i) - '0';
+        int carry = 0;
+        int[] arr = new int[num1.length() + num2.length()];
 
-            for (int j = n - 1; j >= 0; j--) {
-                int d2 = num2.charAt(j) - '0';
-                int mul = d1 * d2;
+        for(int i = num2.length() - 1; i >= 0; i--){
+            char ch = num2.charAt(i);
+            int count1 = ch - '0';
 
-                int p1 = i + j;
-                int p2 = i + j + 1;
+            for(int j = num1.length() - 1; j >= 0; j--){
+                char ch2 = num1.charAt(j);
+                int count2 = ch2 - '0';
 
-                int sum = mul + res[p2];
+                int mul = count1 * count2;
+                int sum = arr[i + j + 1] + mul + carry;
 
-                res[p2] = sum % 10;
-                res[p1] += sum / 10;
+                carry = sum / 10;
+                arr[i + j + 1] = sum % 10;
             }
+
+            arr[i] += carry;
+            carry = 0;
         }
 
         StringBuilder sb = new StringBuilder();
+        int index = 0;
 
-        for (int num : res) {
-            if (!(sb.length() == 0 && num == 0)) {
-                sb.append(num);
+        for(int l = 0; l < arr.length; l++){
+            if(arr[l] == 0){
+                index = l;
+            } else {
+                break;
             }
         }
 
-        return sb.length() == 0 ? "0" : sb.toString();
+        if(index == arr.length - 1){
+            return "0";
+        }
+
+        if(arr[index] == 0){
+            index++;
+        }
+
+        for(int k = index; k < arr.length; k++){
+            sb.append(arr[k]);
+        }
+
+        return sb.toString();
     }
 }
