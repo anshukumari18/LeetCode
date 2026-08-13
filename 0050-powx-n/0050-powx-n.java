3857d42d11534dbf3366b5d36982476e1b8d308f
Anshu Kumari <anshukumari82524@gmail.com>
2026-08-13 13:12:29 +0530

    Time: 1 ms (9.84%), Space: 48.2 MB (22.83%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,22 +1,20 @@
 class Solution {
     public double myPow(double x, int n) {
-        double ans=1;
-        long n_temp = n;
-        if(n_temp<0){
-            n_temp *= -1;
-        }
-        while(n_temp>0){
-            if(n_temp%2==0){
-                x = x*x;
-                n_temp = n_temp/2;
-            }else{
-                ans = ans * x;
-                n_temp--;
+        double ans=1.0;
+        long nn=n;
+        if(nn<0) nn=-1*nn;
+        while(nn>0){
+            if(nn%2==1){
+                ans = ans *x;
+                nn=nn-1;
+            }
+            else{
+                x=x*x;
+                nn=nn/2;
             }
         }
-        if(n<0){
-            return (double) 1/ans;
-        }
+        if(n<0)
+        ans = (double)(1.0)/(double)(ans);
         return ans;
     }
 }
