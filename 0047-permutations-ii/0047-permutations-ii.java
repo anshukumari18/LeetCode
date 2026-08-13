540d87b7efad009895fed4b031855fd4eb332575
Anshu Kumari <anshukumari82524@gmail.com>
2026-08-13 12:49:04 +0530

    Time: 4 ms (27.95%), Space: 46.9 MB (29.61%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,29 +1,32 @@
 class Solution {
-    public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        Arrays.sort(nums); 
-        boolean[] used = new boolean[nums.length];
-        backtrack(nums, new ArrayList<>(), res, used);
-        return res;
-    }
-
-    private void backtrack(int[] nums, List<Integer> current, 
-                           List<List<Integer>> res, boolean[] used) {
-        if (current.size() == nums.length) {
-            res.add(new ArrayList<>(current));
+    public static void helper(List<Integer> num, List<List<Integer>> res, List<Integer> temp) {
+        if (num.size() == 0) {
+            res.add(new ArrayList<>(temp));
             return;
         }
-        for (int i = 0; i < nums.length; i++) {
-            
-            if (used[i]) continue;
+        for (int i = 0; i < num.size(); i++) {
+            while (i + 1 < num.size() && num.get(i) == num.get(i + 1)) {
+                i += 1;
+            }
+            temp.add(num.get(i));
 
-            if (i > 0 && nums[i] == nums[i-1] && !used[i-1]) continue;
+            List<Integer> newNums = new ArrayList<>(num);
+            newNums.remove(i);
 
-            current.add(nums[i]);
-            used[i] = true;
-            backtrack(nums, current, res, used);
-            current.remove(current.size() - 1);
-            used[i] = false;
+            helper(newNums, res, temp);
+            temp.remove(temp.size() - 1);
         }
+    }
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        List<Integer> num = new ArrayList<>();
+        for (int x : nums) {
+            num.add(x);
+        }
+        helper(num, res, temp);
+        return res;
     }
 }
