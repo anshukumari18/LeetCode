daf04f67acc7348a235b88dae57fcdeff6369b0b
Anshu Kumari <anshukumari82524@gmail.com>
2026-08-13 12:13:21 +0530

    Time: 54 ms (25.01%), Space: 47.2 MB (68.35%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,44 +1,34 @@
 class Solution {
-    Boolean[][] dp;
-
     public boolean isMatch(String s, String p) {
-        dp = new Boolean[s.length() + 1][p.length() + 1];
-        return match(s, p, 0, 0);
-    }
+        int n = p.length();
+        int m = s.length();
+        boolean[] prev= new boolean[m+1]; 
+        prev[0] = true;
+        for(int j =1 ; j <= m ; j++){
+            prev[j]=false;
+        }
 
-    private boolean match(String s, String p, int i, int j) {
-
-        // Memo check
-        if (dp[i][j] != null)
-            return dp[i][j];
-
-        // Pattern finished
-        if (j == p.length())
-            return dp[i][j] = (i == s.length());
-
-        // String finished
-        if (i == s.length()) {
-            for (int k = j; k < p.length(); k++) {
-                if (p.charAt(k) != '*')
-                    return dp[i][j] = false;
+        for(int i =1 ; i<=n ; i++){
+            boolean[] curr= new boolean[m+1];
+            boolean flag = true;
+            for(int ii =1 ; ii<=i ; ii++){
+                if(p.charAt(ii-1)!= '*'){
+                    flag = false;
+                    break;
+                }
             }
-            return dp[i][j] = true;
+            curr[0]=flag;
+            for(int j =1 ; j<=m ; j++){
+                if(p.charAt(i-1)==s.charAt(j-1) || p.charAt(i-1)=='?'){
+                    curr[j] = prev[j-1];
+                }else if(p.charAt(i-1)=='*'){
+                    curr[j] = prev[j] || curr[j-1];
+                }else{
+                    curr[j] = false;
+                }
+            }
+            prev=curr;
         }
-
-        char pc = p.charAt(j);
-
-        // Case 1: '*'
-        if (pc == '*') {
-            return dp[i][j] =
-                    match(s, p, i, j + 1) ||   // zero characters
-                    match(s, p, i + 1, j);     // one or more characters
-        }
-
-        // Case 2: '?' or exact match
-        if (pc == '?' || s.charAt(i) == pc) {
-            return dp[i][j] = match(s, p, i + 1, j + 1);
-        }
-
-        return dp[i][j] = false;
+        return prev[m];
     }
 }
