d9d9e277515d61af7dd260b693a0f25a1c4ab1cd
Anshu Kumari <anshukumari82524@gmail.com>
2026-08-13 12:52:17 +0530

    Time: 4 ms (27.95%), Space: 46.6 MB (59.81%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,26 +1,24 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        if(nums==null || nums.length==0) return res;
-        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
         boolean[] used = new boolean[nums.length];
-        find(used,res,new ArrayList<>(),nums);
-        return res;
+        Arrays.sort(nums);
+        solve(nums, used, new ArrayList<>(), ans);
+        return ans;
     }
 
-    private void find(boolean used[], List<List<Integer>> res, List<Integer> temp, int[] nums){
-        if(temp.size() == nums.length){   // we find a permutation so add it to result
-            res.add(new ArrayList<>(temp));
+    private void solve(int[] nums, boolean[] used,  List<Integer> tempList, List<List<Integer>> ans){
+        if(tempList.size() == nums.length){
+            ans.add(new ArrayList<>(tempList));
             return;
         }
+
         for(int i=0; i<nums.length; i++){
-            if(used[i]) continue;
-            if(i>0 && nums[i]==nums[i-1] && !used[i-1]) continue;
-                
-            temp.add(nums[i]);    // add the curr element (Take condition)
+            if(used[i] || (i > 0 && nums[i] == nums[i-1] && used[i-1])) continue;
+            tempList.add(nums[i]);
             used[i] = true;
-            find(used,res,temp,nums);  // recurse to find permutations with the current temp
-            temp.remove(temp.size()-1); // remove the last element (Not Take condition)
+            solve(nums, used, tempList, ans);
+            tempList.remove(tempList.size() - 1);
             used[i] = false;
         }
     }
