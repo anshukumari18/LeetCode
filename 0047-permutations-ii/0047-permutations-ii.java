9a6af016cc5e98f838237cdebd0ad7ff69a26dde
Anshu Kumari <anshukumari82524@gmail.com>
2026-08-13 12:43:29 +0530

    Time: 3 ms (91.85%), Space: 46.5 MB (88.92%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,30 +1,31 @@
 class Solution {
-    public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> res=new ArrayList<>();
-        List<Integer> temp=new ArrayList<>();
-        Set<List<Integer>> distinct=new HashSet<>();
-        Set<Integer> visited=new HashSet<>();
-        helper(0, nums, distinct, visited, temp);
-        res.addAll(distinct);
-        return res;
-    }
-
-    public void helper(int i, int[] nums, Set<List<Integer>> distinct, Set<Integer> visited, List<Integer> temp) {
-        if(i==nums.length)
-        {
-            distinct.add(new ArrayList<>(temp));
+    private void permuteUniqueUtil(int[] nums, List<List<Integer>> ans, List<Integer> ds, boolean[] used){
+        if(ds.size() == nums.length){
+            ans.add(new ArrayList<>(ds));
             return;
         }
-        for(int k=0;k<nums.length;k++)
-        {
-            if(!visited.contains(k))
-            {
-                visited.add(k);
-                temp.add(nums[k]);
-                helper(i+1, nums, distinct, visited, temp);
-                temp.remove(temp.size()-1);
-                visited.remove(k);
+
+        for(int i=0; i<nums.length; i++){
+            if(used[i]) continue;
+
+            if(i>0 && nums[i-1] == nums[i] && !used[i-1]) continue; //Skip a duplicate number if its previous identical number is not used yet.
+
+            if(!used[i]){
+                used[i] = true;
+                ds.add(nums[i]);
+                permuteUniqueUtil(nums, ans, ds, used);
+                ds.remove(ds.size()-1);
+                used[i]= false;
             }
         }
+    }
+    public List<List<Integer>> permuteUnique(int[] nums) {
+       List<List<Integer>> ans = new ArrayList<>();
+       List<Integer> ds = new ArrayList<>();
+       boolean[] used = new boolean[nums.length];
+       //Important 
+       Arrays.sort(nums);
+       permuteUniqueUtil(nums, ans, ds, used);
+       return ans;
     }
 }
