ca6c4db925ddb42ec69f47c8e37e4fe36f000aa2
Anshu Kumari <anshukumari82524@gmail.com>
2026-08-13 12:40:40 +0530

    Time: 6 ms (15.87%), Space: 46.2 MB (98.32%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,24 +1,31 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        Arrays.sort(nums);
-        permu(ans, new ArrayList<>(), nums, new boolean[nums.length]);
-        return ans;
+        List<List<Integer>> res=new ArrayList<>();
+        List<Integer> temp=new ArrayList<>();
+        Map<Integer, Integer> map=new HashMap<>();
+        for(int num:nums)
+        {
+            map.put(num, map.getOrDefault(num, 0)+1);
+        }
+        helper(nums, res, map, temp);
+        return res;
     }
 
-    private void permu(List<List<Integer>> ans, List<Integer> subans, int[] nums, boolean[] b) {
-        if(subans.size() == nums.length){
-            ans.add(new ArrayList<>(subans));
-        } else {
-            for(int i = 0; i < nums.length; i++) {
-                if(b[i] || i > 0 && nums[i] == nums[i-1] && !b[i - 1]){
-                    continue;
-                }
-                b[i] = true;
-                subans.add(nums[i]);
-                permu(ans, subans, nums, b);
-                b[i] = false;
-                subans.remove(subans.size() - 1);
+    public void helper(int[] nums, List<List<Integer>> res, Map<Integer, Integer> map, List<Integer> temp) {
+        if(temp.size()==nums.length)
+        {
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+        for(int num:map.keySet())
+        {
+            if(map.get(num)>0)
+            {
+                temp.add(num);
+                map.put(num, map.get(num)-1);
+                helper(nums, res, map, temp);
+                temp.remove(temp.size()-1);
+                map.put(num, map.get(num)+1);
             }
         }
     }
