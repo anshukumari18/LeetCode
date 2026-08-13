fa451f024033ad58dc5f0fdc9f914f6bf3a2d224
Anshu Kumari <anshukumari82524@gmail.com>
2026-08-13 12:47:26 +0530

    Time: 21 ms (13.83%), Space: 47.2 MB (9.5%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,29 +1,43 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        boolean[] used = new boolean[nums.length];
-        Arrays.sort(nums); 
-        backtrack(nums, used, new ArrayList<>(), result);
-        return result;
+        List<List<Integer>> res = new ArrayList<>();
+        HashSet<List<Integer>> set = new HashSet<>();
+
+        backtrack(res, nums, 0, set);
+        return res;
     }
 
-    private void backtrack(int[] nums, boolean[] used, List<Integer> path, List<List<Integer>> result) {
-        if (path.size() == nums.length) {
-            result.add(new ArrayList<>(path));
-            return;
+    public void backtrack(List<List<Integer>> res, int[] nums, int index, HashSet<List<Integer>> set) {
+        if (index == nums.length) {
+            List<Integer> current = toList(nums);
+            if (!set.contains(current)) {
+                res.add(current);
+                set.add(current);
+            }
+        } else {
+            for (int i = index; i < nums.length; i++) {
+                swap(index, i, nums);
+                backtrack(res, nums, index + 1, set);
+                swap(index, i, nums);
+            }
         }
+    }
 
-        for (int i = 0; i < nums.length; i++) {
-            if (used[i]) continue;
-            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
+    public void swap(int m, int n, int[] nums) {
+        int temp = nums[m];
+        nums[m] = nums[n];
+        nums[n] = temp;
+    }
 
-            used[i] = true;
-            path.add(nums[i]);
-
-            backtrack(nums, used, path, result);
-
-            path.remove(path.size() - 1);
-            used[i] = false;
+    public List<Integer> toList(int[] nums) {
+        ArrayList<Integer> l = new ArrayList<>();
+        for (int n : nums) {
+            l.add(n);
         }
+        return l;
     }
 }
