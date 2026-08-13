4b056c354ce7f29cd35618e72ad61ea79c247336
Anshu Kumari <anshukumari82524@gmail.com>
2026-08-13 12:07:43 +0530

    Time: 2 ms (93.06%), Space: 45.5 MB (77.05%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,25 +1,32 @@
 class Solution {
-    public boolean isMatch(final String s, final String p) {
-        int i = 0, j = 0, star = -1, match = -1;
-
-        while(i < s.length()) {
-            if(j < p.length() && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '?')) {
-                i++;
+    public boolean isMatch(String s, String p) {
+        int i=0,j=0,si=-1,m=0;
+        char S[]=s.toCharArray();
+        char P[]=p.toCharArray();
+        int ns=s.length();
+        int np=p.length();
+        while(i<ns){
+            if(j<np&&(S[i]==P[j]||P[j]=='?')){
                 j++;
-            } else if(j < p.length() && p.charAt(j) == '*') {
-                star = j++;
-                match = i;
-            } else if(star != -1) {
-                j = star + 1;
-                i = ++match;
-            } else {
+                i++;
+            }
+            else if(j<np&&P[j]=='*'){
+                si=j;
+                m=i;
+                j++;
+            }
+            else if(si!=-1){
+                j=si+1;
+                m++;
+                i=m;
+            }
+            else{
                 return false;
             }
         }
-
-        while(j < p.length() && p.charAt(j) == '*')
+        while(j<np&&P[j]=='*'){
             j++;
-
-        return i == s.length() && j == p.length();
+        }
+        return j==np;
     }
 }
