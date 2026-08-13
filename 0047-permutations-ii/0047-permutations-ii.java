7a4fd9e5871628cb7aa4bf1da7edb335ddefb0d1
Anshu Kumari <anshukumari82524@gmail.com>
2026-08-13 12:51:35 +0530

    Time: 3 ms (91.85%), Space: 46.8 MB (29.61%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,28 +1,26 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        if(nums==null || nums.length==0) return res;
+        Arrays.sort(nums);
         boolean[] used = new boolean[nums.length];
-        Arrays.sort(nums); 
-        backtrack(nums, used, new ArrayList<>(), result);
-        return result;
+        find(used,res,new ArrayList<>(),nums);
+        return res;
     }
 
-    private void backtrack(int[] nums, boolean[] used, List<Integer> path, List<List<Integer>> result) {
-        if (path.size() == nums.length) {
-            result.add(new ArrayList<>(path));
+    private void find(boolean used[], List<List<Integer>> res, List<Integer> temp, int[] nums){
+        if(temp.size() == nums.length){   // we find a permutation so add it to result
+            res.add(new ArrayList<>(temp));
             return;
         }
-
-        for (int i = 0; i < nums.length; i++) {
-            if (used[i]) continue;
-            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
-
+        for(int i=0; i<nums.length; i++){
+            if(used[i]) continue;
+            if(i>0 && nums[i]==nums[i-1] && !used[i-1]) continue;
+                
+            temp.add(nums[i]);    // add the curr element (Take condition)
             used[i] = true;
-            path.add(nums[i]);
-
-            backtrack(nums, used, path, result);
-
-            path.remove(path.size() - 1);
+            find(used,res,temp,nums);  // recurse to find permutations with the current temp
+            temp.remove(temp.size()-1); // remove the last element (Not Take condition)
             used[i] = false;
         }
     }
