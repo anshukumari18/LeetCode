a256f3d54d8d0fd973577999dc3e6e87ec41b510
Anshu Kumari <anshukumari82524@gmail.com>
2026-08-13 12:48:16 +0530

    Time: 3 ms (91.85%), Space: 46.7 MB (59.81%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,43 +1,29 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        HashSet<List<Integer>> set = new HashSet<>();
-
-        backtrack(res, nums, 0, set);
+        Arrays.sort(nums); 
+        boolean[] used = new boolean[nums.length];
+        backtrack(nums, new ArrayList<>(), res, used);
         return res;
     }
 
-    public void backtrack(List<List<Integer>> res, int[] nums, int index, HashSet<List<Integer>> set) {
-        if (index == nums.length) {
-            List<Integer> current = toList(nums);
-            if (!set.contains(current)) {
-                res.add(current);
-                set.add(current);
-            }
-        } else {
-            for (int i = index; i < nums.length; i++) {
-                swap(index, i, nums);
-                backtrack(res, nums, index + 1, set);
-                swap(index, i, nums);
-            }
+    private void backtrack(int[] nums, List<Integer> current, 
+                           List<List<Integer>> res, boolean[] used) {
+        if (current.size() == nums.length) {
+            res.add(new ArrayList<>(current));
+            return;
         }
-    }
+        for (int i = 0; i < nums.length; i++) {
+            
+            if (used[i]) continue;
 
-    public void swap(int m, int n, int[] nums) {
-        int temp = nums[m];
-        nums[m] = nums[n];
-        nums[n] = temp;
-    }
+            if (i > 0 && nums[i] == nums[i-1] && !used[i-1]) continue;
 
-    public List<Integer> toList(int[] nums) {
-        ArrayList<Integer> l = new ArrayList<>();
-        for (int n : nums) {
-            l.add(n);
+            current.add(nums[i]);
+            used[i] = true;
+            backtrack(nums, current, res, used);
+            current.remove(current.size() - 1);
+            used[i] = false;
         }
-        return l;
     }
 }
