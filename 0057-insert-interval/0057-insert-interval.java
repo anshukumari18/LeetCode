5d5d41cad65b0548638e14c6c0a4074c560e7b75
Anshu Kumari <anshukumari82524@gmail.com>
2026-08-14 11:30:16 +0530

    Time: 1 ms (98.15%), Space: 46.9 MB (76.02%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,31 +1,26 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> result = new ArrayList<>();
-        int i = 0;
-
-        // Add all intervals that come **before** the new interval
-        while (i < intervals.length && intervals[i][1] < newInterval[0]) {
-            result.add(intervals[i]);
+        ArrayList<int []> al=new ArrayList<>();
+        int i=0;
+        for(i=0;i<intervals.length;i++){
+            if(intervals[i][1]<newInterval[0]){
+                al.add(intervals[i]);
+            }
+            else if(intervals[i][0]>newInterval[1]){
+                break;
+            }
+            else{
+                newInterval[0]=Math.min(intervals[i][0],newInterval[0]);
+                newInterval[1]=Math.max(intervals[i][1],newInterval[1]);
+            }
+        }
+        al.add(newInterval);
+        
+        while(i<intervals.length){
+            al.add(intervals[i]);
             i++;
         }
+        return al.toArray(new int[al.size()][]);
 
-        // **Merge** the new interval with all overlapping intervals
-        while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
-            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
-            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
-            i++;
-        }
-        result.add(newInterval);
-
-        // Add all intervals that come **after** the merged interval
-        while (i < intervals.length) {
-            result.add(intervals[i]);
-            i++;
-        }
-
-        return result.toArray(new int[result.size()][]);
     }
 }
