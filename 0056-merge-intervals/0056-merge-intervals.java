e1d085b6cb37b4fe5b7c0236d0b6d6d2639b0649
Anshu Kumari <anshukumari82524@gmail.com>
2026-08-14 11:18:42 +0530

    Time: 9 ms (36.45%), Space: 48.8 MB (92.65%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,35 +1,21 @@
-import java.util.*;
-
 class Solution {
     public int[][] merge(int[][] intervals) {
+        
+        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
 
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        List<int[]> ans = new ArrayList<>();
 
-        List<int[]> ans=new ArrayList<>();
+        for(int[] interval : intervals) {
 
-        for(int i=0;i<intervals.length;i++){
-            
-      
-
-            if(ans.isEmpty()||intervals[i][0]>ans.get(ans.size()-1)[1]){
-          
-              ans.add(new int[]{intervals[i][0],intervals[i][1]});
-             
-            }
-
-            else{
-                ans.get(ans.size()-1)[1]=Math.max(ans.get(ans.size()-1)[1],intervals[i][1]);
-            }
-
-
+        if(ans.isEmpty() || interval[0] > ans.get(ans.size() - 1)[1]){
+            ans.add(interval);
+        }
+        else {
+            ans.get(ans.size() - 1)[1] = Math.max(ans.get(ans.size() - 1)[1], interval[1]);
         }
 
+        } 
 
-        return ans.toArray(new int [ans.size()][]);
-
-
-         
-     
-
+        return ans.toArray(new int[ans.size()][]);
     }
 }
