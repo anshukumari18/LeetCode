3538577c7dc3d58222535b0de5badccad0ad52b6
Anshu Kumari <anshukumari82524@gmail.com>
2026-08-14 11:27:14 +0530

    Time: 3 ms (99.4%), Space: 48.9 MB (75.7%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,21 +1,53 @@
 class Solution {
-    public int[][] merge(int[][] intervals) {
-        
-        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
-
-        List<int[]> ans = new ArrayList<>();
-
-        for(int[] interval : intervals) {
-
-        if(ans.isEmpty() || interval[0] > ans.get(ans.size() - 1)[1]){
-            ans.add(interval);
+   public int[][] merge(int[][] intervals) {
+        int numberOfMerging = 0;
+        int [][] result = new int[intervals.length][2];
+        quicksort(intervals, 0, intervals.length - 1);
+        int j = 0;
+        if(intervals.length != 0){
+            result[0][0] = intervals[0][0];
+            result[0][1] = intervals[0][1];
         }
-        else {
-            ans.get(ans.size() - 1)[1] = Math.max(ans.get(ans.size() - 1)[1], interval[1]);
+        for (int i = 1; i < intervals.length; i++) {
+            if(result[j][1] >= intervals[i][0]){
+                result[j][1] = Math.max(result[j][1], intervals[i][1]);
+                numberOfMerging++;
+                continue;
+            }
+            j++;
+            result[j][0] = intervals[i][0];
+            result[j][1] = intervals[i][1];
         }
+        int [][] finishResult = new int[intervals.length - numberOfMerging][2];
+        for (int i = 0; i < finishResult.length; i++) {
+            finishResult[i][0] = result[i][0];
+            finishResult[i][1] = result[i][1];
+        }
+        return finishResult;
+    }
 
-        } 
+    private static void quicksort (int [][] arr, int low, int high){
+        if(arr.length == 0 || high < low) return;
+        int mid = low + (high - low) / 2;
 
-        return ans.toArray(new int[ans.size()][]);
+        int midEl = arr[mid][0];
+        int i = low, j = high;
+        while(i <= j){
+            while(arr[i][0] < midEl) i++;
+            while(arr[j][0] > midEl) j--;
+
+            if(i <= j) {
+                int temp = arr[i][0];
+                int temp2 = arr[i][1];
+                arr[i][0] = arr[j][0];
+                arr[i][1] = arr[j][1];
+                arr[j][0] = temp;
+                arr[j][1] = temp2;
+                i++;
+                j--;
+            }
+        }
+        quicksort(arr, low ,j);
+        quicksort(arr, i, high);
     }
 }
