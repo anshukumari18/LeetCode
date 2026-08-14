d814c4471b809a5186d2d81f36843e1774593802
Anshu Kumari <anshukumari82524@gmail.com>
2026-08-14 11:40:44 +0530

    Time: 2 ms (40.75%), Space: 42.7 MB (88.32%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.java
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -1,28 +1,21 @@
 class Solution {
     public String getPermutation(int n, int k) {
-        if(n == 1) return "1";
+        List<Integer> nums = new ArrayList<>();
+        for (int i = 1; i <= n; i++) nums.add(i);
+
+        int[] fact = new int[n];
+        fact[0] = 1;
+        for (int i = 1; i < n; i++) fact[i] = fact[i - 1] * i;
+
+        k--;
         StringBuilder sb = new StringBuilder();
-        for(int i = 1; i <= n; i++) sb.append(i);
-        int factorial = 1;
-        for(int i = 2; i < n; i++) factorial *= i;
-        n--;
 
-        StringBuilder result = new StringBuilder();
-        while(k > 0) {
-            if(k % factorial == 0) {
-                result.append(sb.charAt((k / factorial) - 1));
-                sb.deleteCharAt((k / factorial) - 1);
-            } else {
-                result.append(sb.charAt(k / factorial));
-                sb.deleteCharAt(k / factorial);
-            } 
-
-            k %= factorial;
-            factorial /= n;
-            n--;
+        for (int i = n; i >= 1; i--) {
+            int idx = k / fact[i - 1];
+            sb.append(nums.get(idx));
+            nums.remove(idx);
+            k %= fact[i - 1];
         }
-
-        for(int i = sb.length() - 1; i >= 0; i--) result.append(sb.charAt(i));
-        return result.toString();
+        return sb.toString();
     }
 }
