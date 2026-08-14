71b641f7b3e3a3da63787bc3d61e876bfd2db8ec
Anshu Kumari <anshukumari82524@gmail.com>
2026-08-14 11:41:44 +0530

    Time: 0 ms (100%), Space: 42.8 MB (75.49%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.java
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -1,21 +1,35 @@
 class Solution {
-    public String getPermutation(int n, int k) {
-        List<Integer> nums = new ArrayList<>();
-        for (int i = 1; i <= n; i++) nums.add(i);
-
-        int[] fact = new int[n];
-        fact[0] = 1;
-        for (int i = 1; i < n; i++) fact[i] = fact[i - 1] * i;
-
-        k--;
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = n; i >= 1; i--) {
-            int idx = k / fact[i - 1];
-            sb.append(nums.get(idx));
-            nums.remove(idx);
-            k %= fact[i - 1];
+    private static int[] fact = {0, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880};
+    
+    private String getPermutation(int n, int k, boolean[] nums, char[] str, int index) {
+        int i = 0, m = nums.length;
+        if(n == 1) {
+            while(i < m && nums[i]) ++i;
+            str[index++]=(char)('0'+i+1);
+            return String.valueOf(str);
         }
-        return sb.toString();
+        if(k == 0) {
+            while(i < m) {
+                if(!nums[i]) str[index++]=(char)('0'+i+1);
+                ++i;
+            }
+            return String.valueOf(str);
+        }
+        
+        int div = k/fact[n-1], mod = k%fact[n-1], j = -1;
+        while(i < m-1 && div != j) {
+            if(!nums[i]) ++j;
+            if(j == div) break;
+            ++i;
+        }
+        str[index++]=(char)('0'+i+1);
+        if(i < m) nums[i]=true;
+        return getPermutation(n-1, mod, nums, str, index); 
+    }
+
+    public String getPermutation(int n, int k) {
+        boolean[] nums = new boolean[n];
+        char[] charArr = new char[n];
+        return getPermutation(n, k-1, nums, charArr, 0);
     }
 }
