799fe79b5dd645d72908d9c92feb96ceff2d8961
Anshu Kumari <anshukumari82524@gmail.com>
2026-08-14 11:38:55 +0530

    Time: 1645 ms (5.01%), Space: 284.5 MB (5.06%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.java
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -1,18 +1,27 @@
 class Solution {
+    List<String> result = new ArrayList<>();
+    boolean[] isVisited;
+
+    private void solve(int n, StringBuilder permutation) {
+        if (permutation.length() == n) {
+            result.add(permutation.toString());
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (!isVisited[i]) {
+                isVisited[i] = true;
+                permutation.append(i);
+                solve(n, permutation);
+                isVisited[i] = false;
+                permutation.deleteCharAt(permutation.length() - 1);
+            }
+        }
+    }
+
     public String getPermutation(int n, int k) {
-        int i=1,fact=1; k-=1;
-        List<Integer> l=new ArrayList<>();
-        StringBuilder sb=new StringBuilder();
-        for(i=1;i<=n;i++){
-            l.add(i); fact*=i;
-        }
-        for(i=n;i>=1;i--){
-            fact/=i;
-            int x=k/fact;
-            sb.append(l.get(x));
-            l.remove(x);
-            k%=fact;
-        }
-        return sb.toString();
+        isVisited = new boolean[n + 1];
+        solve(n, new StringBuilder());
+        return result.get(k - 1);
     }
 }
