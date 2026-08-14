b698533ea7749ece795f0702252e0f2bd5ecf34e
Anshu Kumari <anshukumari82524@gmail.com>
2026-08-14 11:33:56 +0530

    Time: 1 ms (98.15%), Space: 47.3 MB (7.31%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,36 +1,76 @@
 class Solution {
+    enum OPERATION {
+        LTE,
+        GTE
+    }
     public int[][] insert(int[][] intervals, int[] newInterval) {
-
-        List<int []>ansArray=new ArrayList<>();
-        int i=0;
-        while(i<intervals.length && intervals[i][1]<newInterval[0])
-        {
-            ansArray.add(intervals[i]);
-            i++;
+        if (intervals.length == 0) {
+            return new int[][] {newInterval};
         }
 
-        int []temp=new int[2];
-        temp[0]=newInterval[0];
-        temp[1]=newInterval[1];
-        while(i<intervals.length && newInterval[1]>=intervals[i][0])
-        {
-            temp[0]=Math.min(temp[0],intervals[i][0]);
-            temp[1]=Math.max(temp[1],intervals[i][1]);
-            i++;
-        }
-        ansArray.add(temp);
-        while(i<intervals.length)
-        {
-            ansArray.add(intervals[i]);
-            i++;
-        }
+        // Get the inclusive indices of overlapping indices to be merged.
+        int lI = binarySearch(intervals, newInterval[0], OPERATION.LTE);
+        int rI = binarySearch(intervals, newInterval[1], OPERATION.LTE);
 
-        int [][]newInsertInterval=new int[ansArray.size()][];
+        //If lI = -1 means we didnt find it.
+        int mergeStartIdx = lI, mergeEndIdx = rI;
+        int insertAt = -1;
         
-        for(int idx=0;idx<ansArray.size();idx++)
-        {
-            newInsertInterval[idx]=ansArray.get(idx);
+        //If newInterval[0] is not present in intervals, find the intervals which can be merged 
+        //with newInteval[0]; Accordingly set the insertAt (or) mergeStart.
+        if (lI == -1) {
+            insertAt = 0;
+            if (rI >= 0) {
+                mergeStartIdx = 0;
+            }
+        } else if (intervals[lI][1] < newInterval[0]) {
+            insertAt = lI+1;
+            mergeStartIdx = (lI +1 > rI) ? -1 : lI + 1;
         }
-        return newInsertInterval;
+        
+        //Based on the insertAt or mergeStart Indices fill the mergeable/insertable
+        int[] mergedInterval = new int[] {};
+        int mergedCount = 0;
+        
+        if (mergeStartIdx != -1 && mergeStartIdx <= mergeEndIdx) {
+            mergedInterval = new int[]{Math.min(intervals[mergeStartIdx][0], newInterval[0]),
+                    Math.max(intervals[mergeEndIdx][1], newInterval[1])};
+            mergedCount = (mergeEndIdx - mergeStartIdx + 1);
+        } else {
+            mergedInterval = newInterval;
+        }
+
+        //Fill the result by just appending the prefixArray + mergedArray/insertedArray + suffixArray
+        int[][] intervalsResult = new int[intervals.length - mergedCount + 1][2];
+
+        for (int i = 0; i < intervalsResult.length; i++) {
+            if (i == insertAt || i == mergeStartIdx) {
+                intervalsResult[i] = mergedInterval;
+                
+            } else if (i < insertAt || i < mergeStartIdx) {
+                intervalsResult[i] = intervals[i];
+            } else {
+                intervalsResult[i] = intervals[mergedCount + i -1];
+            }
+        }
+        return intervalsResult;
+    }
+
+    private int binarySearch(int[][] intervals, int lookFor, OPERATION operation) {
+        int l = 0, r = intervals.length -1;
+        if (intervals[0][0] > lookFor) {
+            return operation.equals(OPERATION.LTE) ? -1 : 0;
+        }
+        while ( l <= r) {
+            int m = (l + r + 1)/2;
+            if (intervals[m][0] < lookFor) {
+                l = m + 1;
+            } else if (intervals[m][0] > lookFor) {
+                r = m - 1;
+            } else {
+                return m;
+            }
+        }
+        return operation.equals(OPERATION.LTE) ? r : l;
     }
 }
