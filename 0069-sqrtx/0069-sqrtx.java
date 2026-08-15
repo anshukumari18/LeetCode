a831b40f34a28d7a2c90f1f429ffcd85002300b6
Anshu Kumari <anshukumari82524@gmail.com>
2026-08-15 23:41:07 +0530

    Time: 1 ms (98.08%), Space: 42.8 MB (8.56%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,25 +1,17 @@
 class Solution {
     public int mySqrt(int x) {
-        // Edge case: square root of 0 is 0
         if (x == 0) return 0;
-
         int left = 1, right = x;
-        int ans = 0;
-
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            long square = (long) mid * mid;
-
-            if (square == x) {
-                return mid; // perfect square
-            } else if (square < x) {
-                ans = mid; // store as possible answer
-                left = mid + 1; // look for bigger
+            if (mid == x / mid) {
+                return mid;
+            } else if (mid < x / mid) {
+                left = mid + 1;
             } else {
-                right = mid - 1; // look for smaller
+                right = mid - 1;
             }
         }
-
-        return ans; // closest integer sqrt
+        return right;
     }
 }
