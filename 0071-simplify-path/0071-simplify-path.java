6fd3c799a14abdf21fdc0fda8b9ac49d95f37dc0
Anshu Kumari <anshukumari82524@gmail.com>
2026-08-16 11:46:42 +0530

    Time: 2 ms (99.84%), Space: 45 MB (50.09%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,24 +1,31 @@
 class Solution {
     public String simplifyPath(String path) {
-      String[] parts=path.split("/");
-      StringBuilder spath=new StringBuilder();
-       for(String part: parts){
-           if(part.isEmpty() || part.equals(".")){
-               continue;
-           }
-           else if(part.equals("..")){
-               if(spath.length()>0){
-                   int lastindex=spath.lastIndexOf("/");
-                   spath.delete(lastindex,spath.length());
-               }
-           }
-           else{
-               spath.append("/").append(part);
-           }
-       }  
-       if(spath.length()==0){
-           spath.append("/");
-       }
-       return spath.toString();
+        String[] spath = new String[path.length()];
+        int top = -1, i = 0, n = path.length();
+        while (i < n) {
+            while (i < n && path.charAt(i) == '/')    ++i;
+            int e = i;
+            while (e < n && path.charAt(e) == '.')    ++e;
+            if (e > i && ((e == n && e - i == 1) || (e < n && e - i == 1 && path.charAt(e) == '/'))) {
+                i = e;
+                continue;
+            }
+            if (e > i && ((e == n && e - i == 2) || (e < n && e - i == 2 && path.charAt(e) == '/'))) {
+                if (top != -1)    --top;
+                i = e;
+                continue;
+            }
+            if (i == n)    break;
+            int s = i;
+            while (i < n && path.charAt(i) != '/')    ++i;
+            spath[++top] = path.substring(s, i);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (i = 0; i <= top; ++i) {
+            sb.append("/");
+            sb.append(spath[i]);
+        }
+        if (sb.length() == 0)   sb.append("/");
+        return sb.toString();
     }
 }
