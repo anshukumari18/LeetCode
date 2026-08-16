f3f7fae4a94f2d4bdf10ec984ea8444e306ecfb6
Anshu Kumari <anshukumari82524@gmail.com>
2026-08-16 11:49:35 +0530

    Time: 1 ms (97.48%), Space: 47.5 MB (62.65%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,47 +1,66 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int m = matrix.length, n = matrix[0].length;
-        boolean firstRowZero = false, firstColZero = false;
-
-        // Check if the first row and first column need to be zero
-        for (int i = 0; i < m; i++) {
-            if (matrix[i][0] == 0) firstColZero = true;
+        boolean firstrowhaszero=false;
+        boolean firstcolhaszero=false;
+        int row=matrix.length;
+        int col=matrix[0].length;
+         // STEP1:CHECK FIRST ROW HAS ZERO 
+        for(int j=0;j<col;j++){
+          if(matrix[0][j]==0){
+            firstrowhaszero=true;
+            break;
+          }
         }
-        for (int j = 0; j < n; j++) {
-            if (matrix[0][j] == 0) firstRowZero = true;
+        //  STEP2:CHECK FIRST COL HAS ZEROES
+        for(int i=0;i<row;i++){
+            if(matrix[i][0]==0){
+                firstcolhaszero=true;break;
+            }
         }
-
-        // Mark zeros in the first row and column
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
+        // STEP3:NOW USE THE FIRST ROW AND COL AS A MARKER FOR FILLING THE ENTIRE ROW WITH ZEROES
+        // IF WE FOUND ANY ZERO INBETWEEN THE ROWS AND COLUMNS WITH ZEROES THEN MARK THE FIRST ROW&COL WITH ZERO
+        for(int i=1;i<row;i++){
+            for(int j=1;j<col;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
                 }
             }
         }
-
-        // Set matrix cells to zero based on markers
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
-                    matrix[i][j] = 0;
-                }
+        // STEP4:NOW FILL ZEROES WITH THE MARKERSS......
+        for(int i=1;i<row;i++){
+            for(int j=1;j<col;j++){
+                if(matrix[i][0]==0||matrix[0][j]==0)
+                     matrix[i][j]=0;
             }
         }
-
-        // Zero out the first row if needed
-        if (firstRowZero) {
-            for (int j = 0; j < n; j++) {
-                matrix[0][j] = 0;
+        //STEP-5:NOW FILL FIRST ROW IF FIRST ROW HAS ZERO
+        if(firstrowhaszero){
+            for(int j=0;j<col;j++){
+                matrix[0][j]=0;
             }
         }
-
-        // Zero out the first column if needed
-        if (firstColZero) {
-            for (int i = 0; i < m; i++) {
-                matrix[i][0] = 0;
+        //STEP-6:NOW FILL FIRST COL IF FIRST COL HAS ZERO
+        if(firstcolhaszero){
+            for(int i=0;i<row;i++){
+                matrix[i][0]=0;
             }
         }
-    }
-}
+    }}
+
+
+        
+
+        
+
+
+
+
+
+
+
+
+
+
+
+      
