81b83caab76f06cae28b43178cf69b3d384358aa
Anshu Kumari <anshukumari82524@gmail.com>
2026-08-16 11:45:45 +0530

    Time: 5 ms (51.8%), Space: 44.4 MB (96.98%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,24 +1,24 @@
 class Solution {
     public String simplifyPath(String path) {
-        Stack<String> stack = new Stack<>();
-// Example: /.../a/../b/c/../d/./
-        String[] arr = path.split("/");
-//Split result: ["", "...", "a", "..", "b", "c", "..", "d", ".", ""]
-
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].equals("") || arr[i].equals(".")) continue;
-            else if (arr[i].equals("..")) {
-                if (!stack.isEmpty()) stack.pop();
-            } else {
-                stack.push(arr[i]);
-            }
-        }
-
-        StringBuilder res = new StringBuilder();
-        for (String s : stack) {
-            res.append("/").append(s);
-        }
-
-        return res.isEmpty() ? "/" : res.toString();
+      String[] parts=path.split("/");
+      StringBuilder spath=new StringBuilder();
+       for(String part: parts){
+           if(part.isEmpty() || part.equals(".")){
+               continue;
+           }
+           else if(part.equals("..")){
+               if(spath.length()>0){
+                   int lastindex=spath.lastIndexOf("/");
+                   spath.delete(lastindex,spath.length());
+               }
+           }
+           else{
+               spath.append("/").append(part);
+           }
+       }  
+       if(spath.length()==0){
+           spath.append("/");
+       }
+       return spath.toString();
     }
 }
