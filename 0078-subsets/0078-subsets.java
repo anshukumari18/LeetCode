5927b9b6ad89e690c82f74770595db9e94f97dce
Anshu Kumari <anshukumari82524@gmail.com>
2026-08-18 02:38:31 +0530

    Time: 1 ms (86.46%), Space: 44 MB (76%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,25 +1,17 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> subsets = new ArrayList<>();
-        Stack<Integer> stk = new Stack<>();
-        getSubsets(stk, 0, nums, subsets);
-        
-        
-        return subsets;
-    }
-    
-    
-    public void getSubsets(Stack<Integer> stk, int startIdx, int[] nums, List<List<Integer>> subsets) {
-        subsets.add(new ArrayList<>(stk));
-        
-        
-        for (int i = startIdx; i < nums.length; i++) {
-            stk.push(nums[i]);            
-            getSubsets(stk, i + 1, nums, subsets);
-            stk.pop();
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+
+        for(int num : nums){
+            int n = outer.size();
+            for(int i = 0; i < n; i++){
+                // Create a new subset from existing one
+                List<Integer> internal = new ArrayList<>(outer.get(i));
+                internal.add(num);
+                outer.add(internal); // Add it to the list
+            }
         }
-        
-        
-        return;
+        return outer;
     }
 }
